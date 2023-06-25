1647b4c4bca8e4b85e8f42da1715cbf2bdb6ace6
Vedant <birlavedant13@gmail.com>
2023-06-25 23:02:53 +0530

    third change

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://one-cart-frontend.vercel.app/"],
     credentials: true,
   })
 );
